13e03a3b130e6dd7374f4ba7d3fb5caacca4f39e
mohitj27 <mjain8156@gmail.com>
2018-06-28 15:21:04 +0530

    Add redux-intermediate

--- redux-intermediate/src/containers/list_of_cars.js
+++ redux-intermediate/src/containers/list_of_cars.js
@@ -2,16 +2,28 @@ import React,{ Component} from 'react';
 import {connect} from 'react-redux';
 
 class ListOfCars extends Component {
+    listOfCars = ({cars}) =>{
+        if(cars){
+            return cars.map((car) =>{
+                return(
+                    <div className = "car_item" >
+                      {car.model}
+                        </div>
+                )
+            })
+        }
+    }
     render(){
         return(
             <div>
-                cars
+                {this.listOfCars(this.props)}
                 </div>
         )
     }
 }
 
 function mapStateToProps(state) {
+     console.log(state);
     return{
         cars:state.cars
     }
